a0ed3af65e5f1ceb4ee5b1fc8169d86ee83f13e0
AfterRace <uolcher@gmail.com>
2015-03-21 16:18:09 +0100

    Aggiunta interfaccia funzione

--- printbootp.c
+++ printbootp.c
@@ -1,6 +1,6 @@
 
-
-printbootp (char op, char htype, char hlen) {
+printbootp(char op,char htype, char hlen, char hops, char *xid, char *secs, char *flags, char *ciaddr, char *yiaddr, char *siaddr, char* giaddr, char* chaddr, char* sname, char* file, char *options, int len_opt){
+//printbootp (char op, char htype, char hlen) { Deprecato
 
 	printf ("Tipo operazione: %d\n", op);
 	//printf ("Ip del server: "); printip(siaddr); printf("\n);
