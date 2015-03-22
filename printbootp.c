a7f16b65c1a1706cddcc80e0fd6d72efc2f35b43
Giulio Riberto <giulio.ribe@gmail.com>
2015-03-22 19:23:08 +0100

    campo optional: inserimento case dhcp message type

--- printbootp.c
+++ printbootp.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
-#include <string.h>
 
+#define DEBUG
 printbootp(unsigned char op, unsigned char htype, unsigned char hlen, unsigned char hops, const unsigned char *xid, const unsigned char *secs, const unsigned char *flags, const unsigned char *ciaddr, const unsigned char *yiaddr, const unsigned char *siaddr, const unsigned char* giaddr, const unsigned char* chaddr, const unsigned char* sname, const unsigned char* file, const unsigned char *options, int len_opt){
 	
-	int i;
+	int i, i_opt, len, y, value;
 
 	if(op == 1)
 		printf("Message op code / message type, value: %d -> BOOTREQUEST\n", op);
@@ -47,6 +47,9 @@ printbootp(unsigned char op, unsigned char htype, unsigned char hlen, unsigned c
 		case 20:
 			printf("Hardware address type, value: %d -> Serial Line\n", htype);
 			break;
+		default:
+			printf("ERRORE: switch htype");
+			break;
 	}
 
 	printf("Hardware address length, value: %d\n", hlen);
@@ -57,14 +60,12 @@ printbootp(unsigned char op, unsigned char htype, unsigned char hlen, unsigned c
 	else
 		printf("\n");
 	
-	// TODO: da controllare se e' stato stampato correttamente
 	printf("Transaction Identifier, value: ");	
 	for(i=0; i<4; i++) {
 		printf("%d", xid[i]);
 	}
 	printf("\n");
 	
-	// TODO: da controllare se e' corretto che il primo valore sia negativo
 	printf("Seconds elapsed since client began address acquisition or renewal process, values: %d - %d\n", secs[0], secs[1]);
 
 	printf("Flags, value broadcast flag (1 bit): ");
@@ -110,10 +111,88 @@ printbootp(unsigned char op, unsigned char htype, unsigned char hlen, unsigned c
 		printf("%d", file[i]);
 	printf("\n");
 
+#ifdef DEBUG
 	printf("Lunghezza campo options: %d\n", len_opt);	
 	for(i=0; i<len_opt; i++)
-		printf("%d", options[i]);
+		printf("%d ", options[i]);
 	printf("\n");
+#endif
+
+	printf("Print options fields:\n");
+	i_opt=0;
+	while(i_opt<len_opt) {
+#ifdef DEBUG
+		
+		printf("Valore options: %d\n", options[i_opt]);
+#endif
+		switch(options[i_opt]) {
+			case 53:
+				// Aggiungo valore
+				i_opt++;
+				
+				
+				i_opt += len;
+				value = options[i_opt];
+#ifdef DEBUG
+				printf("len: %d\n", len);
+				printf("Value: %d\n", value);			
+#endif
+				printf("DHCP Message Type: ");
+				switch(value) {
+					case 1:
+						printf("DHCPDISCOVER\n");
+						break;
+					case 2:
+						printf("DHCPOFFER\n");
+						break;
+					case 3:
+						printf("DHCPREQUEST\n");
+						break;
+					case 4:
+						printf("DHCPDECLINE\n");
+						break;
+					case 5:
+						printf("DHCPACK\n");
+						break;
+					case 6:
+						printf("DHCPNAK\n");
+						break;
+					case 7:
+						printf("DHCPRELEASE\n");
+						break;
+					case 8:
+						printf("DHCPINFORM\n");
+						break;
+					default:
+						printf("ERRORE: DHCP Message Type\n");
+						break;
+				}
+				i_opt++;
+				break;
+			case 54:
+				// Aumento di uno perche' ho letto il tipo di campo opzionale
+				i_opt++;
+				// Aumento di 4 perche' vado a leggere un indirizzo ip
+				i_opt += 4;
+				printf("Server identifier: ");
+				for(i=i_opt; i<i_opt; i++) {
+#ifdef DEBUG
+					printf("\n Valore i_opt: %d\n", i_opt);
+					printf("Valore i: %d\n", i);
+#endif
+					printf("%d", options[i]);
+					if(i<(i_opt-1))
+						printf(".");
+					else
+						printf("\n");
+				}
+				break;
+			default:
+				printf("ERRORE: options fields\n");
+				return(-1);
+				break;
+		}
+	}
 
 
 	//printf ("Ip del server: "); printip(siaddr); printf("\n);
