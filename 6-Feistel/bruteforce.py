5b4cf366b2fc1421bb3c23aadc7a9267ab6823d9
giulioribe <giulio.ribe@gmail.com>
2015-04-29 10:23:32 +0200

    bruteforce complete

--- 6-Feistel/bruteforce.py
+++ 6-Feistel/bruteforce.py
@@ -1,18 +1,34 @@
 import itertools
 import utils
 import time
+import sys
+import marco
 
 if __name__ == "__main__":
+    if len(sys.argv) == 4:
+        len_key = int(sys.argv[1])
+        filename_padded = sys.argv[2]
+        filename_encrypt = sys.argv[3]
+    elif not len(sys.argv) == 1:
+        sys.exit("Per eseguire correttamente: 'python bruteforce.py len_key filename_padded filename_encrypt'")
     print "Welcome to the party\n" 
     print "Inizio l'attacco"
+    print "Attendi..."
+    filename_encrypt = "file-crypted.tga"
+    filename_padded = "file-padded.tga"
     start = time.time()
-    for x in map(''.join, itertools.product('01', repeat=10)):
-        # chiamo la funzione di Marco
-        print "Calcolo l'md5 dei file per vedere se sono uguali"
-        print "Attendi..."
-        if md5same(md5('original_file'), md5('encrypt_file')):
+    for key in map(''.join, itertools.product('01', repeat=len_key)):
+        filename_decrypt = marco.create_decrypted_file(key, filename_encrypt)
+        md5_padded = utils.md5(filename_padded)
+        md5_decrypt = utils.md5(filename_decrypt)    
+        if utils.md5same(md5_padded, md5_decrypt):
+            print
+            print "md5 del file '" + filename_padded + "':\t" + md5_padded
+            print "md5 del file '" + filename_decrypt + "':\t" + md5_decrypt
             time = round(time.time() - start, 2)
-            print "La chiave del tesoro e': " + str(x) + ", lunghezza " + str(len(x)) + " bit"
+            print "La chiave del tesoro e': '" + str(key) + "', lunghezza " + str(len(key)) + " bit"
             print "E' stata trovata in: " + str(time) + " secondi"
             break
+        else:
+            print "Ho provato con chiave: '" + str(key) + "'"
     print "\nJob done, goodbye"
--- 6-Feistel/utils.py
+++ 6-Feistel/utils.py
@@ -2,10 +2,10 @@ import hashlib
 
 def md5same(md5_1, md5_2):
     if md5_1 == md5_2:
-        print "Yeah, i due file sono uguali"
+        #print "Yeah, i due file sono uguali"
         return True
     else:
-        print "Ops, i due file non sono uguali -> ritenta, sarai piu' fortunato"
+        #print "Ops, i due file non sono uguali -> ritenta, sarai piu' fortunato"
         return False
     
 def md5(filename):
