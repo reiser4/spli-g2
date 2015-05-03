b1c57a4d1e1c4774d504f3a0b9f7bd0fea6d9eba
giulioribe <giulio.ribe@gmail.com>
2015-05-03 16:20:43 +0200

    add functions in 'utils.py'

--- 7-Massey.Omura/utils.py
+++ 7-Massey.Omura/utils.py
@@ -1,13 +1,15 @@
 import hashlib
-from fractions import gcd
+import fractions
 import sys
 import pyprimes
 
 def md5same(md5_1, md5_2):
     if md5_1 == md5_2:
-        print "Yeah, i due file sono uguali"
+        #print "Yeah, i due file sono uguali"
+        return True
     else:
-        sys.exit("Ops, i due file non sono uguali -> EXIT")
+        #sys.exit("Ops, i due file non sono uguali -> EXIT")
+        return False
     
 def md5(filename):
     return hashlib.md5(open(filename, 'rb').read()).hexdigest()
@@ -30,6 +32,24 @@ def modinv(a, m):
     else:
         return x % m
 
+def calculateP(n):
+    """
+    Prende in ingresso un numero 'n' 
+    restituisce il numero primo successivo a 'n'
+    """
+    return next(pyprimes.primes_above(n))
+    
+def calculateEncryptionKey(nthprime, p):
+    """
+    Prende in ingresso la posizione del numero primo che si vuole calcolare (se passo 10, prendo il decimo numero primo) e 'p'
+    restituisce 'e' ovvero un numero primo diverso da p-1 e che sia primo con quest'ultimo
+    """
+    e = pyprimes.nth_prime(nthprime)
+    fp = p-1
+    while e == fp or fractions.gcd(e, fp) != 1:
+        nthprime += 2
+        e = pyprimes.nth_prime(nthprime)
+    return e
 
 if __name__ == "__main__":
     print modinv(43, 103)
